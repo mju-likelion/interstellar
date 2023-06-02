29eff4823b119f4b11bec5b9ec285349113f58ae
ndaemy <yuyaebean@gmail.com>
2023-06-03 00:12:14 +0900

    chore: eslint member-ordering 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,9 +20,24 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/member-ordering': [
+      'error',
+      {
+        default: [
+          // 수정 필요하시면 수정하고 공유만 해주세여
+          'private-field',
+          'protected-field',
+          'public-field',
+          'constructor',
+          'private-instance-method',
+          'protected-instance-method',
+          'public-instance-method',
+        ],
+      },
+    ],
     '@typescript-eslint/no-explicit-any': 'off',
     'import/order': [
       'error',

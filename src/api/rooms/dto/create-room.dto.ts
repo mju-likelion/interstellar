d9abb9dd3aa44adce4bc69fb4b6d7e99c27bdd84
ndaemy <yuyaebean@gmail.com>
2023-07-07 01:49:48 +0900

    feat: Room 생성 validation 에 에러 메세지들 추가

--- src/api/rooms/dto/create-room.dto.ts
+++ src/api/rooms/dto/create-room.dto.ts
@@ -1,18 +1,27 @@
 import { IsBoolean, IsOptional, Matches } from 'class-validator';
 
 export class CreateRoomDto {
-  @Matches(/^(\d{4}-\d{2}-\d{2},?)+$/)
+  @Matches(/^(\d{4}-\d{2}-\d{2},?)+$/, {
+    message:
+      '올바른 날짜 형식이 아닙니다. 다음 형식을 지켜주세요. (YYYY-MM-DD,YYYY-MM-DD,...)',
+  })
   dates: string;
 
   @IsOptional()
-  @IsBoolean()
+  @IsBoolean({
+    message: 'dateOnly는 boolean 형식이어야 합니다.',
+  })
   dateOnly?: boolean;
 
   @IsOptional()
-  @Matches(/^([01]\d|2[0-3]):([0-5]\d)$/)
+  @Matches(/^([01]\d|2[0-3]):([0-5]\d)$/, {
+    message: '올바른 시간 형식이 아닙니다. 다음 형식을 지켜주세요. (HH:MM)',
+  })
   startTime?: string;
 
   @IsOptional()
-  @Matches(/^([01]\d|2[0-3]):([0-5]\d)$/)
+  @Matches(/^([01]\d|2[0-3]):([0-5]\d)$/, {
+    message: '올바른 시간 형식이 아닙니다. 다음 형식을 지켜주세요. (HH:MM)',
+  })
   endTime?: string;
 }

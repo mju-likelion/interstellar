6b5d825621e87af533821b52363677aa6aae0370
ndaemy <yuyaebean@gmail.com>
2023-07-07 22:43:13 +0900

    chore: project root 에서부터 relative import 가능하도록

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { RoomsModule } from './api/rooms/rooms.module';
-import { UsersModule } from './api/users/users.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { RoomsModule } from '@/api/rooms/rooms.module';
+import { UsersModule } from '@/api/users/users.module';
+import { PrismaModule } from '@/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule, RoomsModule, UsersModule],

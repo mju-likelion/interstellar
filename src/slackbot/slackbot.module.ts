c367ad3c8362f6e31c1469926ee6e0e7680ba193
k013600 <k012123600@gmail.com>
2023-09-07 21:32:06 +0900

    fix: 테스트하던 코드를 안지웠네요

--- src/slackbot/slackbot.module.ts
+++ src/slackbot/slackbot.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 
 import { SlackbotService } from './slackbot.service';
-import { SlackbotController } from './slackbot.controller';
 
 @Module({
-  controllers: [SlackbotController],
   providers: [SlackbotService],
   exports: [SlackbotService],
 })
